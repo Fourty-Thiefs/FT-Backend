d77066530db70e68e6eced2bd57cf90491cad4ba
tam le van <tamle.dev@gmail.com>
2023-07-11 23:54:06 +0700

    Tam: Added schedule service to handle background job

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,15 @@ import { HomeController } from './modules/homemodule/home.controller';
 import { HomeService } from './modules/homemodule/home.service';
 import { UserController } from './modules/usermodule/user.controller';
 import { UserService } from './modules/usermodule/user.service';
+import { ScheduleService } from './service/schedule/background-job.schedule';
+
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [],
+  imports: [
+    ScheduleModule.forRoot(),
+  ],
   controllers: [AppController, HomeController, UserController],
-  providers: [AppService, HomeService, UserService],
+  providers: [AppService, HomeService, UserService, ScheduleService],
 })
 export class AppModule {}
---
+++ src/service/schedule/background-job.schedule.ts
@@ -0,0 +1,12 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+
+@Injectable()
+export class ScheduleService {
+  private readonly logger = new Logger(ScheduleService.name);
+
+  @Cron('30 * * * * *')
+  handleCron() {
+    this.logger.debug('Called when the current second is 30s');
+  }
+}
